f6e0d29e0ec4623a10796e5d157d8cc3ec8ef1ba
Eugenia Leong <el@eugenialeong.com>
2016-01-28 23:53:08 -0800

    Quick Instagram client

---
+++ Instagram/PhotoCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoCell.swift
+//  Instagram
+//
+//  Created by Eugenia Leong on 1/28/16.
+//  Copyright © 2016 Eugenia Leong. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
